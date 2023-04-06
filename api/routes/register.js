ce55297743b1df67ace03539647e47a2ee5ef439
rarescho <91527949+rarescho@users.noreply.github.com>
2023-04-06 10:24:44 +0200

    ok login

--- api/routes/register.js
+++ api/routes/register.js
@@ -63,14 +63,7 @@ router.post("/", (request, response) => {
           });
         });
       });
-      // })
-      // // catch error if the password hash isn't successful
-      // .catch((e) => {
-      //   response.status(500).send({
-      //     message: "ERROR pw",
-      //     e,
-      //   });
-      // });
+      
   });
 router.post("/check", async(req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
