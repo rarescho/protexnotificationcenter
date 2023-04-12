5c428920d026d5d3eaff53fc3cf346f4b0b00507
rarescho <91527949+rarescho@users.noreply.github.com>
2023-04-12 16:51:07 +0200

    rimozione app con expo

--- protexnotificationcenter/App.js
+++ protexnotificationcenter/App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {theme} from './src/core/theme';
+import {
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  Dashboard,
+} from './src/screens';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider theme={theme}>
+        {/* <RemotePushController /> */}
+
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="LoginScreen"
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+
   );
 }
 
---
+++ protexnotificationcenter/src/screens/Dashboard.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { StatusBar, SafeAreaView } from "react-native";
+import Timeline from "react-native-beautiful-timeline";
+import axios from 'axios';
+
+
+class DashBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notifications: [],
+      selected: null
+    };
+    this.onEventPress = this.onEventPress.bind(this);
+    this.renderSelected = this.renderSelected.bind(this);
+  }
+  // http://10.99.99.167:9000/notification/xuser
+  async componentDidMount() {
+    let data = JSON.stringify({
+      "auth_protex": "CHORARES"
+    });
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://www.protex-dashboard.it/api/notification/xuser',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin' : '*',
+        'Accept': 'application/json'
+
+      },
+      data : data
+    };
+    console.log(config);
+    axios.request(config)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log("ERRORE:",error);
+    })
+    .finnaly(() =>{
+
+    });
+
+    // await fetch('https://www.protex-dashboard.it/api/notification/xuser', {
+    //   method: 'POST',
+    //   mode: 'cors',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json;charset=UTF-8',
+    //     'Access-Control-Allow-Origin' : '*',
+    //   },
+    //   body: JSON.stringify({
+    //     auth_protex: 'CHORARES',
+    //   }),
+    // })   
+    //   .then(response => response.json())
+    //   .then(data => this.setState({notifications: data}))
+    //   .catch(error => console.error(error));
+  }
+
+  onEventPress(data) {
+    this.setState({selected: data});
+  }
+
+  renderSelected() {
+    if(this.state.selected) {
+      return <Text style={{marginTop:0}}>Selected event: {this.state.selected.title} at {this.state.selected.time}</Text>
+    }
+  }
+
+  render() {
+    const { notifications } = this.state;
+    const dummyData = notifications.map(notification => ({
+      date: notification.dataora,
+      data: [
+        {
+          title: notification.title,
+          subtitle: notification.subtitle,
+          date: notification.dataora,
+        }]
+    }));
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: "#fdfdfd", padding: 16 }}
+        >              
+          <Timeline
+            data={dummyData}
+            dashThickness={2}
+            dashLength={dummyData.length}
+          />
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+
+export default DashBoard;
