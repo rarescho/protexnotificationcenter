286191316063c59c5c527e60b65851fc4ed2abc1
rarescho <91527949+rarescho@users.noreply.github.com>
2023-05-12 11:32:48 +0200

    ultimo cliente

---
+++ protexnotificationcenter/src/screens/LoginScreen.js
@@ -0,0 +1,83 @@
+import React, { useState,useEffect } from 'react'
+import { TouchableOpacity, StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import TextInput from '../components/TextInput'
+import BackButton from '../components/BackButton'
+import { theme } from '../core/theme'
+import { emailValidator } from '../helpers/emailValidator'
+import { passwordValidator } from '../helpers/passwordValidator'
+import { notifications, NotificationMessage, Android } from 'react-native-firebase-push-notifications'
+
+
+export default function LoginScreen({ navigation }) {
+  const [username, setUsername] = useState({ value: '', error: '' })
+
+  const onLoginPressed = () => {
+      // setUsername({ ...username, error: usernameError })
+      // return    
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Dashboard' }],
+    })
+  }
+
+
+
+  return (
+    <Background>
+      <BackButton goBack={navigation.goBack} />
+      <Logo />
+      <Header>Benvenuto </Header>
+      <TextInput
+        label="Utente"
+        returnKeyType="next"
+        value={username.value}
+        onChangeText={(text) => setUsername({ value: text, error: '' })}
+        error={!!username.error}
+        errorText={username.error}
+        autoCapitalize="none"
+        keyboardType="default"
+      />
+      <View style={styles.forgotPassword}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('ResetPasswordScreen')}
+        >
+          <Text style={styles.forgot}>Hai dimenticato la password?</Text>
+        </TouchableOpacity>
+      </View>
+      <Button mode="contained" onPress={onLoginPressed}>
+        Login
+      </Button>
+      <View style={styles.row}>
+        <Text>Non hai una chaive di accesso? </Text>
+        <TouchableOpacity onPress={() => navigation.replace('RegisterScreen')}>
+          <Text style={styles.link}>Richiedila</Text>
+        </TouchableOpacity>
+      </View>
+    </Background>
+  )
+}
+
+const styles = StyleSheet.create({
+  forgotPassword: {
+    width: '100%',
+    alignItems: 'flex-end',
+    marginBottom: 24,
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  forgot: {
+    fontSize: 13,
+    color: theme.colors.secondary,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+})
